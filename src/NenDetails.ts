c71bb30f62351a4a748d0d58bc661cf13d04419c
SrorDekel <srordekel@gmail.com>
2023-04-03 11:20:55 -0300

    more nen details

--- src/NenDetails.ts
+++ src/NenDetails.ts
@@ -1,3 +1,4 @@
+import Battle from "./Game/Battle"
 
 
 export type NenSkillName =  'ten' | 'ren' | 'zetsu' | 'gyo' | 'the_in' | 'en' | 'shu' | 'kou' | 'ken' | 'ryo'
@@ -19,6 +20,38 @@ class NenDetails {
         }[skill]
     }
 
+    skillEffectDescription(skill: NenSkillName) {
+        return {
+            ten: 'contain aura: other skills cost less -> enemy skills cost more',
+            ren: 'increase aura: replenish aura every turn / increase skill effectiveness',
+            zetsu: 'stop aura flow: evade aura based attacks',
+            gyo: 'concentrate aura in body part: increase attack / block power',
+            the_in: 'conceal aura: make enemy block less effective -> avoid aura based conditions',
+            en: 'sphere of awareness: your block is more effective',
+            shu: 'give aura to object',
+            kou: 'extreme gyo: drastically increase attack / block, lower defense',
+            ken: 'fortify self: increase defense (even if not blocking)',
+            ryo: 'distribute aura',
+        }[skill]
+    }
+
+    skillEffects(skill: NenSkillName) {
+        const jumpTable: {[k: string]: (b: Battle) => void} = {
+            ten: (b: Battle) => {},
+            ren: (b: Battle) => {},
+            zetsu: (b: Battle) => {},
+            gyo: (b: Battle) => {},
+            the_in: (b: Battle) => {},
+            en: (b: Battle) => {},
+            shu: (b: Battle) => {},
+            kou: (b: Battle) => {},
+            ken: (b: Battle) => {},
+            ryo: (b: Battle) => {},
+        }
+
+        return jumpTable[skill]
+    }
+
     skillBases(skill: NenSkillName) {
         return {
             ten: undefined,
@@ -32,6 +65,17 @@ class NenDetails {
             kou: ['gyo', 'zetsu'],
             en: ['ken']
         }[skill]
+    }
+
+    auraTypeDescription(auraTypeName: AuraTypeName){
+        return {
+            enhancement: 'use aura to strengthen or increase the natural abilities of an object or one\'s own body',
+            manipulation: 'control living or non-living things, including aura constructs',
+            conjuration: 'create a physical, independent, material object out of one\'s aura',
+            specialist: '',
+            transmutation: 'change the properties of their aura to mimic something else or replicate specific attributes.',
+            emission: 'separate aura from body; ranged / long lasting attacks'
+        }[auraTypeName]
     }
 
     auraTypeEffectiveness: (userNenType: AuraTypeName) => ({[k: string]: number}) = userNenType => {
